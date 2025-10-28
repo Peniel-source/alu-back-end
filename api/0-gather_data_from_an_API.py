5cc34510f43f1163f80c66a40bd2496441b41a9d
Peniel Nissi Obeng <p.obeng@alustudent.com>
2025-10-28 15:35:07 +0200

    #

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+import requests
+import sys
+
+employeeId=sys.argv[1]
+
+
+url = f"https://jsonplaceholder.typicode.com/users/{employeeId}"
+
+response = requests.get(url)
+
+data= response.json()
+
+username = data['name']
+# print(username)
+
+
+todo_url = f'https://jsonplaceholder.typicode.com/todos'
+paramas = { "userId" : f"{employeeId}" }
+r = requests.get(todo_url,params=paramas)
+
+dta = r.json()
+
+
+total= len(dta)
+done_tasks = [task for task in dta if task.get('completed')]
+
+print(f'Employee {username} is done with tasks ({len(done_tasks)}/{total}):')
+
+for title in done_tasks:
+    print(f'\t{title.get("title")}')
